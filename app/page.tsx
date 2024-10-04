9ea583d67a2126c262c3f9b417babab8dee4c56c
andyiac <andyiac@m1.local>
2024-10-04 18:32:43 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-10 gap-6 font-[family-name:var(--font-geist-sans)] mt-0">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
+        <h2> prifix is: {prefix}</h2>
+
           <Image
             className="dark:invert"
             src={`${prefix}/images/logo.png`} 
