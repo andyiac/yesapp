e3fba809b00873fbcdea29611fa48bf7e257a922
andyiac <andyiac@m1.local>
2024-10-04 17:42:04 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 
 const nextConfig = {
     output: 'export',
+    images: { unoptimized: true },
     compiler: {
       // Enables the styled-components SWC transform
       styledComponents: true
